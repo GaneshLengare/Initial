a5335c9a0a967aeb23fd9e375ba59bc18b5d999c
GaneshLengare <148607444+GaneshLengare@users.noreply.github.com>
2025-08-01 18:53:04 +0530

    Kolhapurkar

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ public class first {
     public static void main(String[]args){
         System.out.println("upGrad");
         System.out.println("FSD");
-        System.out.println("Ranchya");
+        System.out.println("Randchya");
         
 
 
